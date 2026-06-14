458d711eecd2088897854786bbac6cc5a1c32952
mancc112-rgb <mancc112@gmail.com>
2026-06-14 23:42:51 +0800

    Delete health-rpg-app directory

--- health-rpg-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import HealthRPG from "./HealthRPG.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HealthRPG />
-  </React.StrictMode>
-);
--- health-rpg-app/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // 部署到 Vercel/Netlify 根網域用 "/"；
-  // 若放在子路徑（例如 GitHub Pages 的 /repo/）再改成 "/repo/"
-  base: "/",
-});
